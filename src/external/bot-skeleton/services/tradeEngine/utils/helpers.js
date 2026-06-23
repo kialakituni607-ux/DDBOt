8dff1e849f1485c991cda93177c904d09c9587be
kialakituni607-ux <kialakituni607@gmail.com>
2026-06-23 23:04:20 +0000

    fix: use underlying_symbol in proposal for OTP API compatibility

--- src/external/bot-skeleton/services/tradeEngine/utils/helpers.js
+++ src/external/bot-skeleton/services/tradeEngine/utils/helpers.js
@@ -20,7 +20,7 @@ export const tradeOptionToProposal = (trade_option, purchase_reference) =>
                 purchase_reference,
             },
             proposal: 1,
-            symbol: trade_option.symbol,
+            underlying_symbol: trade_option.symbol,
         };
         if (trade_option.prediction !== undefined) {
             proposal.selected_tick = trade_option.prediction;
