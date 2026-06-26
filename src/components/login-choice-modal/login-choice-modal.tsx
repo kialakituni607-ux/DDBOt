02c3e113054c029432a0bbd6962f31b86592b821
kialakituni607-ux <kialakituni607@gmail.com>
2026-06-26 20:43:43 +0000

    fix: remove login choice modal, go straight to PKCE

--- src/components/login-choice-modal/login-choice-modal.tsx
+++ src/components/login-choice-modal/login-choice-modal.tsx
@@ -1,57 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { redirectToLegacyLogin, derivLogin } from '@/utils/deriv-auth-adapter';
-import './login-choice-modal.scss';
-
-const LOGIN_CHOICE_KEY = 'login_flow_choice';
-
-const LoginChoiceModal = ({ onClose }: { onClose: () => void }) => {
-    const handleNewAPI = () => {
-        localStorage.setItem(LOGIN_CHOICE_KEY, 'pkce');
-        onClose();
-        derivLogin();
-    };
-    const handleLegacy = () => {
-        localStorage.setItem(LOGIN_CHOICE_KEY, 'legacy');
-        onClose();
-        redirectToLegacyLogin();
-    };
-    return (
-        <div className='login-choice-modal__overlay'>
-            <div className='login-choice-modal'>
-                <h2 className='login-choice-modal__title'>Choose Login Method</h2>
-                <p className='login-choice-modal__desc'>Which type of Deriv account do you have?</p>
-                <div className='login-choice-modal__buttons'>
-                    <button className='login-choice-modal__btn login-choice-modal__btn--new' onClick={handleNewAPI}>
-                        <span className='login-choice-modal__btn-title'>New Deriv API</span>
-                        <span className='login-choice-modal__btn-desc'>Recently created account</span>
-                    </button>
-                    <button className='login-choice-modal__btn login-choice-modal__btn--legacy' onClick={handleLegacy}>
-                        <span className='login-choice-modal__btn-title'>Old Deriv Account</span>
-                        <span className='login-choice-modal__btn-desc'>Classic Deriv account</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
-};
+import { derivLogin } from '@/utils/deriv-auth-adapter';
 
 export const useLoginChoice = () => {
-    const [showModal, setShowModal] = useState(false);
-
     const triggerLogin = () => {
-        const saved = localStorage.getItem(LOGIN_CHOICE_KEY);
-        if (saved === 'pkce') {
-            derivLogin();
-        } else if (saved === 'legacy') {
-            redirectToLegacyLogin();
-        } else {
-            setShowModal(true);
-        }
+        derivLogin();
     };
-
-    const modal = showModal ? <LoginChoiceModal onClose={() => setShowModal(false)} /> : null;
-
+    const modal = null;
     return { triggerLogin, modal };
 };
 
-export default LoginChoiceModal;
+export default useLoginChoice;
